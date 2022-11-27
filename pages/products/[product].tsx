77859fab93e9095a5354b36e0db319ede5121327
seiyadragon <34778012+seiyadragon@users.noreply.github.com>
2022-11-27 14:13:21 -0600

    0.0.5

--- pages/products/[product].tsx
+++ pages/products/[product].tsx
@@ -4,6 +4,8 @@ import { createClient, PostgrestResponse } from '@supabase/supabase-js';
 import Link from 'next/link';
 import Image from 'next/image';
 import { GetServerSideProps } from 'next';
+import { useState, MouseEvent } from 'react';
+import { parseJsonText } from 'typescript';
 
 type ProductProps = {
     product?: any
@@ -23,33 +25,94 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     }
 }
 
+type ProductFlavor = {
+    name: string,
+    description: string
+}
+
 export default function Product({product}: ProductProps) {
+    let [displayImage, setDisplayImage] = useState(product.images[0])
+    function onImageLinkClick(event: MouseEvent<HTMLAnchorElement>, image: string) {
+        event.preventDefault();
+        setDisplayImage(image);
+    }
+
+    let [dropDownActive, setDropDownActive] = useState(false)
+    function onDropDownClick(event: MouseEvent<HTMLButtonElement>) {
+        setDropDownActive(!dropDownActive)
+    }
+
+    let [currentFlavor, setCurrentFlavor] = useState(product.flavors[0])
+    function onFlavorClick(event: MouseEvent<HTMLButtonElement>, flavor: ProductFlavor) {
+        setCurrentFlavor(flavor)
+    }
+
     return (
         <main>
             <HeadSettings title="Shop"/>
             <NavBar />
             <BreadCrumb pages={[{name: "HemPyre", link: "/"}, {name: "Shop", link: "/shop"}, {name: product.name, link: "/shop"}]}/>
-            <section className='container'>
-                <section className='section columns has-background-light'>
-                    <section className='column is-2'>
-                        <ul>
-                            {product.images.map((image: string) => {
-                                return (
-                                    <li key={image} className='image is-3by3 link'>
-                                        <section className='box'>
-                                            <Image src={image} alt="Cant Load Image!" width={128} height={128} />
-                                        </section>
-                                    </li>
-                                )
-                            })}
-                        </ul>
-                    </section>
+            <section className='container has-background-light'>
+                <section className='section columns'>
+                    <ul className='column is-2'>
+                        {product.images.map((image: string) => {
+                            return (
+                                <li key={image} className='image is-3by3 link mt-2 mb-2'>
+                                    <Link 
+                                        href={image}
+                                        className= {
+                                            image === displayImage ? 
+                                                'box pt-2 pb-2 pl-2 pr-2 has-background-dark'
+                                            :
+                                                'box pt-2 pb-2 pl-2 pr-2 has-background-white'
+                                        }
+                                        onClick={(event: MouseEvent<HTMLAnchorElement>) => {
+                                            onImageLinkClick(event, image)
+                                        }}
+                                    >
+                                        <Image src={image} alt="Cant Load Image!" width={256} height={256}/>
+                                    </Link>
+                                </li>
+                            )
+                        })}
+                    </ul>
                     <section className='column is-half'>
-                        <span>Yuh</span>
+                        <figure className='image box is-3by3 mt-2'>
+                            <Image src={displayImage} alt="Cant Load Image!" width={1024} height={1024} />
+                        </figure>
                     </section>
                     <section className='column'>
-                        <span>Yuh</span>
+                        <p className='box mt-2'>{product.description}</p>
+                        <section className='box mt-2'>
+                            <section className={dropDownActive ? 'dropdown is-active' : 'dropdown'}>
+                                <button className='button' onClick={onDropDownClick}>Select your favorite flavor</button>
+                                <section className='dropdown-menu'>
+                                    <ul className='dropdown-content'>
+                                        {product.flavors.map((flavor: any) => {
+                                            return (
+                                                <li key={flavor.name}>
+                                                    <button
+                                                        className={flavor.name === currentFlavor ? 'dropdown-item button is-active is-primary' : 'dropdown-item button'}
+                                                        onClick={(event: MouseEvent<HTMLButtonElement>) => {
+                                                            onFlavorClick(event, flavor)
+                                                        }}  
+                                                    >
+                                                        {flavor.name}
+                                                    </button>
+                                                </li>
+                                            )
+                                        })}
+                                    </ul>
+                                </section>
+                            </section>
+                            <br/>
+                            <strong><span className='mt-1'>{currentFlavor.name}</span></strong>
+                            <p className='pt-3'>{currentFlavor.description}</p>
+                        </section>
                     </section>
+                </section>
+                <section className='box mt-0 mb-0 ml-6 mr-6'>
+                    <span className='is-size-4'>{"$" + product.price}</span>
                 </section>
             </section>
         </main>
--- pages/shop.tsx
+++ pages/shop.tsx
@@ -3,38 +3,70 @@ import { NavBar, BreadCrumb, HeadSettings } from '.';
 import { createClient, PostgrestResponse } from '@supabase/supabase-js'
 import Link from 'next/link'
 import Image from 'next/image'
+import { GetServerSideProps } from 'next';
+import { ChangeEvent, KeyboardEvent, useState } from 'react';
+import { useRouter } from 'next/router';
 
 export type ShopProps = {
-    products: Array<any>
+    products: Array<any>,
 }
 
-export async function getServerSideProps() {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    let searchValue: string = (context.query.search === undefined) ? String("") : String(context.query.search!);
+
     let supabaseUrl: string = process.env.SUPABASE_URL!;
     let supabaseKey: string = process.env.SUPABASE_PRIV_KEY!;
 
     let supabase = createClient(supabaseUrl, supabaseKey);
     let {data: products, error}: PostgrestResponse<any> = await supabase.from("Products").select();
 
+    console.log("Search Value:" + searchValue)
+
+    let filteredProducts: Array<any> = []
+    products?.map((product) => {
+        if (product.name.toString().toLocaleLowerCase().includes(searchValue.toString().toLocaleLowerCase()))
+            filteredProducts.push(product)
+    })
+
     return {
         props: {
-            products: products
+            products: filteredProducts
         }
     }
 }
 
 export default function Shop({products}: ShopProps) {
+    let router = useRouter()
+    let [searchBarValue, setSearchBarValue] = useState("")
+
+    function onSearchBarChange(event: ChangeEvent<HTMLInputElement>) {
+        setSearchBarValue(event.target.value)
+    }
+
+    function onSearchBarKeyDown(event: KeyboardEvent) {
+        if (event.key === 'Enter') {
+            router.push("/shop?search=" + searchBarValue)
+        }
+    }
+
     return(
         <main>
             <HeadSettings title="Shop"/>
             <NavBar />
             <BreadCrumb pages={[{name: "HemPyre", link: "/"}, {name: "Shop", link: "/shop"}]}/>
-            <section className='container'>
-                <section className='section pb-0 mb-0 has-background-light'>
+            <section className='container has-background-light'>
+                <section className='section pb-0 mb-0'>
                     <span className='title'>Shop</span>
                     <br />
-                    <input className='input mt-2 pt-0 mb-0 pb-0'></input>
+                    <input
+                        className='input mt-2 pt-0 mb-0 pb-0'
+                        placeholder='Search for products here...'
+                        value={searchBarValue}
+                        onChange={onSearchBarChange}
+                        onKeyDown={onSearchBarKeyDown}
+                    />
                 </section>
-                <ul className='section columns has-background-light'>
+                <ul className='section columns'>
                     {products.map((product) => {
                         return (
                             <li key={product.id} className='column is-2'>
@@ -44,8 +76,8 @@ export default function Shop({products}: ShopProps) {
                                             <span className='card-header-title has-text-centered'>{product.name}</span>
                                         </section>
                                         <section className='card-image is-white'>
-                                            <figure className='image is-1by1'>
-                                                <Image src={product.images[0]} alt="Image not found!" width={128} height={128}/>
+                                            <figure className='image is-3by3'>
+                                                <Image src={product.images[0]} alt="Image not found!" width={256} height={256}/>
                                             </figure>
                                         </section>
                                         <section className='card-footer is-white'>
